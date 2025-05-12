5046d420586daf107dea81703d702799c49219fb
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-05-12 12:50:08 +0300

    Test that the function does not modify punctuation marks

--- spec/ceaser_spec.rb
+++ spec/ceaser_spec.rb
@@ -3,11 +3,16 @@ require_relative "../ceaser_cipher"
 describe "#ceaser_cipher" do
   let(:str) { "What a string!" }
   let(:num) { 5 }
+  let(:result) { "Bmfy f xywnsl!" }
   it "returns a string" do
     expect(ceaser_cipher(str, num)).to be_a(String)
   end
 
   it "Does not return the original string" do
     expect(ceaser_cipher(str, num)).not_to eql(str)
+  end
+  
+  it "Does not modify punctuation marks" do
+    expect(ceaser_cipher(str, num)[-1]).to eql("!")
   end
 end
